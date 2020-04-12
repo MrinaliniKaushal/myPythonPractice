ac06ae3a308f524efc33ff861384943175b648ce
Mrinalini <mrinalinikaushal@gmail.com>
2020-04-12 23:51:57 +0530

    Python basic programs

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Purpose: Say hello
+
+#import argparse
+
+#parser = argparse.ArgumentParser(description='Say hello')
+#parser.add_argument('name', help='Name to greet')
+#args = parser.parse_args()
+#print('Hello, ' + args.name + '!')
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('-n','--name', metavar='name', default='World', help='Name to greet')
+args = parser.parse_args()
+print('Hello, ' + args.name + '!') 
